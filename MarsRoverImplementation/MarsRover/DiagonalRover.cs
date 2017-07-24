bd22d61e81861cde29fde18e01be3c4f328f0a2a
Pratik Kumar <pkumar@tavisca.com>
2017-07-25 02:51:00 +0530

    added diagonal rover class

---
+++ MarsRoverImplementation/MarsRover/DiagonalRover.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    class DiagonalRover : Rover, IRover
+    {
+        public DiagonalRover(PlanetMars mars, int xCordinate = 0, int yCordinate = 0, Direction direction = Direction.North) : base(mars, xCordinate, yCordinate, direction)
+        {
+        }
+    }
+}
---
+++ MarsRoverImplementation/PlanetMarsFixture/DiagonalRoverTester.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PlanetMarsFixture
+{
+    [TestClass]
+    public class DiagonalRoverTester
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
