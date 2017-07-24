f987f075d095d84b9c069a05c43bc915d32c1e6a
Pratik Kumar <pkumar@tavisca.com>
2017-07-25 02:35:55 +0530

    added move functionality for rover

--- MarsRoverImplementation/MarsRover/Rover.cs
+++ MarsRoverImplementation/MarsRover/Rover.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace MarsRover
 {
-    public class Rover
+    public class Rover : IRover
     {
-        public int XCordinate { get; }
-        public int YCordinate { get; }
-        public Direction FacingDirection { get; }
+        public int XCordinate { get; private set; }
+        public int YCordinate { get; private set; }
+        public Direction FacingDirection { get; private set; }
         private PlanetMars mars;
 
         public Rover(PlanetMars mars, int xCordinate = 0, int yCordinate = 0, Direction direction = Direction.North)
@@ -23,8 +23,62 @@ namespace MarsRover
 
         public bool Move(Direction direction)
         {
-            return false;
+            switch (direction)
+            {
+                case Direction.North:
+                    return MoveUp();
+                case Direction.South:
+                    return MoveDown();
+                case Direction.East:
+                    return MoveRight();
+                case Direction.West:
+                    return MoveLeft();
+                default: return false;
+            }
         }
 
+        private bool MoveUp()
+        {
+            FacingDirection = Direction.North;
+            if (mars.IsPositionInsidePlanet(XCordinate, YCordinate - 1))
+            {
+                YCordinate--;
+                return true;
+            }
+            else return false;
+        }
+
+        private bool MoveDown()
+        {
+            FacingDirection = Direction.South;
+            if (mars.IsPositionInsidePlanet(XCordinate, YCordinate + 1))
+            {
+                YCordinate++;
+                return true;
+            }
+            else return false;
+        }
+
+        private bool MoveLeft()
+        {
+            FacingDirection = Direction.West;
+            if (mars.IsPositionInsidePlanet(XCordinate, XCordinate - 1))
+            {
+                XCordinate--;
+                return true;
+            }
+            else return false;
+        }
+
+        private bool MoveRight()
+        {
+            FacingDirection = Direction.East;
+            if (mars.IsPositionInsidePlanet(XCordinate, YCordinate + 1))
+            {
+                XCordinate++;
+                return true;
+            }
+            else return false;
+        }
     }
 }
--- MarsRoverImplementation/PlanetMarsFixture/RoverTester.cs
+++ MarsRoverImplementation/PlanetMarsFixture/RoverTester.cs
@@ -37,14 +37,65 @@ namespace PlanetMarsFixture
             var mars = new PlanetMars(1, 1);
             Rover rover = new Rover(mars);
             var roverMoved = rover.Move(Direction.North);
+            Assert.AreEqual(Direction.North, rover.FacingDirection);
             Assert.IsFalse(roverMoved);
         }
+
         [TestMethod]
         public void Check_if_rover_moves_south_from_initial_position()
         {
             Rover rover = new Rover(new PlanetMars());
             var roverMoved = rover.Move(Direction.South);
+            Assert.AreEqual(Direction.South, rover.FacingDirection);
             Assert.IsTrue(roverMoved);
+        }
+
+        [TestMethod]
+        public void Move_rover_north_by_2_steps()
+        {
+            Rover rover = new Rover(new PlanetMars(), 5, 5);
+            int inititalY = rover.YCordinate;
+            rover.Move(Direction.North);
+            rover.Move(Direction.North);
+            int finalY = rover.YCordinate;
+            Assert.AreEqual(inititalY - 2, finalY);
+            Assert.AreEqual(Direction.North, rover.FacingDirection);
+        }
+
+        [TestMethod]
+        public void Move_rover_south_by_2_steps()
+        {
+            Rover rover = new Rover(new PlanetMars(), 5, 5);
+            int inititalY = rover.YCordinate;
+            rover.Move(Direction.South);
+            rover.Move(Direction.South);
+            int finalY = rover.YCordinate;
+            Assert.AreEqual(inititalY + 2, finalY);
+            Assert.AreEqual(Direction.South, rover.FacingDirection);
+        }
+
+        [TestMethod]
+        public void Move_rover_in_all_directions_in_single_tile_planet()
+        {
+            Rover rover = new Rover(new PlanetMars(1, 1));
+            rover.Move(Direction.North);
+            rover.Move(Direction.South);
+            rover.Move(Direction.East);
+            rover.Move(Direction.West);
+            Assert.IsTrue(rover.XCordinate == 0 && rover.YCordinate == 0);
+            Assert.AreEqual(Direction.West, rover.FacingDirection);
+        }
+
+        [TestMethod]
+        public void Move_rover_in_all_directions()
+        {
+            Rover rover = new Rover(new PlanetMars(), 5, 5);
+            rover.Move(Direction.North);
+            rover.Move(Direction.South);
+            rover.Move(Direction.East);
+            rover.Move(Direction.West);
+            Assert.IsTrue(rover.XCordinate == 5 && rover.YCordinate == 5);
+            Assert.AreEqual(Direction.West, rover.FacingDirection);
         }
     }
 }
